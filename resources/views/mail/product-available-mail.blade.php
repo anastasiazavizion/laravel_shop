2b2f0a73985bb9efe741993d39647b2a2c2f5810
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-08-07 19:09:29 +0200

    Changed email text

--- resources/views/mail/product-available-mail.blade.php
+++ resources/views/mail/product-available-mail.blade.php
@@ -1,12 +1,15 @@
 <x-mail::message>
-# Introduction
+    # Introduction
 
-The body of your message.
+    <x-mail::panel>
+        <div>Hello, {{$user->name}} {{$user->lastname}}</div>
+        <div>Product {{$product->title}} is available now!</div>
+    </x-mail::panel>
 
-<x-mail::button :url="''">
-Button Text
-</x-mail::button>
+    <x-mail::button :url="$url">
+        {{$product->title}}
+    </x-mail::button>
 
-Thanks,<br>
-{{ config('app.name') }}
+    Thanks,<br>
+    {{ config('app.name') }}
 </x-mail::message>
