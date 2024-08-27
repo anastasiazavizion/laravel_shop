ed0cd1f68d81931c31b56da3a17f76e3d3a93707
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-08-27 17:34:50 +0200

    Reviews function

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -9,6 +9,7 @@ import product from './product.js';
 import cart from './cart.js';
 import order from './order.js';
 import user from './user.js';
+import review from './review.js';
 
 const store = createStore({
     modules: {
@@ -20,6 +21,7 @@ const store = createStore({
         cart,
         order,
         user,
+        review
     },
     plugins: [createPersistedState()]
 });
