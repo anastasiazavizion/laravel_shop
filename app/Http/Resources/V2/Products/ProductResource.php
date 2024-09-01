d95e23a3ea24f23206573eab3a6650505b7f60e3
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-01 14:40:05 +0200

    Test

--- app/Http/Resources/V2/Products/ProductResource.php
+++ app/Http/Resources/V2/Products/ProductResource.php
@@ -19,7 +19,7 @@ class ProductResource extends JsonResource
         return [
                 'id' => $this->id,
                 'title' => $this->title,
-                'title6' => $this->title,
+                'title7' => $this->title,
                 'description' => $this->description,
                 'price' => $this->price,
                 'quantity' => $this->quantity,
