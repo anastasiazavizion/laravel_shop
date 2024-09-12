5220253d49390e681450b01731229234295fe334
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-12 15:01:04 +0200

    test sanctum

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,localhost:8087,127.0.0.1,127.0.0.1:8000,::1',
+        'localhost,localhost:3000,localhost:8087,https://young-sierra-98491-6deaabeac9d3.herokuapp.com,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
