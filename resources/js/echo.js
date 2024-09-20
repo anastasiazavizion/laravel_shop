1d2552a54d94682ed414c37f29fbb9ac2b5dd101
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-20 15:25:36 +0200

    Pusher

--- resources/js/echo.js
+++ resources/js/echo.js
@@ -19,6 +19,7 @@ window.Echo = new Echo({
 window.Echo.private('admin-channel')
     .listen('.admin.order.created', (e) => {
         console.log(e);
+        console.log(e.total);
         const toast = useToast();
         toast("You have new order with total "+e.total);
     });
