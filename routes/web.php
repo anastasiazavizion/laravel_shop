942b05aaa819d00422228aa28cce265e5e519c47
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-20 15:34:28 +0200

    Cleared code

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,6 @@
 <?php
-
-use App\Enum\WishListType;
-use App\Events\Admin\AdminOrderCreatedEvent;
 use Illuminate\Support\Facades\Route;
 
-//$pr = \Illuminate\Support\Facades\Cache::get('product_63');
-
-//$pr2 = \App\Models\Product::find(63);
-
-//dd($pr2->isWishedProduct(WishListType::EXIST->value));
-
-
-
-//dd(/*$pr->is_in_wish_list_exist,$pr->is_in_wish_list_price,*/$pr2->is_in_wish_list_exist,$pr2->is_in_wish_list_price);
-
-
-/*dd(Cache::get('products_page_1_1_params_d751713988987e9331980363e24189ce'));*/
-
-
-
 Route::get('{any}', function () {
-
-    AdminOrderCreatedEvent::dispatch(100);
-
     return view('app');
 })->where('any', '.*');
