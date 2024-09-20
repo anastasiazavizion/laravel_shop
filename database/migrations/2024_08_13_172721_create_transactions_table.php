122fc8086fd32130bc2e24f9034774a766d6d3c0
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-20 16:57:57 +0200

    Fixed migrations

--- database/migrations/2024_08_13_172721_create_transactions_table.php
+++ database/migrations/2024_08_13_172721_create_transactions_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use App\Models\Order;
 return new class extends Migration
 {
     /**
@@ -13,9 +13,7 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-
-            $table->foreignIdFor(\App\Models\Order::class)->constrained('orders');
-
+            $table->foreignIdFor(Order::class)->constrained('orders')->cascadeOnDelete();
             $table->string('payment_system');
             $table->string('status');
             $table->timestamps();
