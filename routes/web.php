b0d257d143cb52e2ea93155d1fbab7f998664bc3
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-21 18:18:49 +0200

    Removed tests

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,6 @@
 <?php
-
-use App\Notifications\NewOrderCreatedNotification;
 use Illuminate\Support\Facades\Route;
 
 Route::get('{any}', function () {
-
-    $order = \App\Models\Order::first();
-
-    if($order){
-        $order->notify(new NewOrderCreatedNotification());
-    }
-
     return view('app');
 })->where('any', '.*');
