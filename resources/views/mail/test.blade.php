598f041762ef99a8a3f3394a8849be09da2211fb
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-21 14:03:53 +0200

    Test mail

--- resources/views/mail/test.blade.php
+++ resources/views/mail/test.blade.php
@@ -1,10 +1,1 @@
-<x-mail::message>
-    # Introduction
-
-    <x-mail::panel>
-     Test message
-    </x-mail::panel>
-
-    Thanks,<br>
-    {{ config('app.name') }}
-</x-mail::message>
+<h1>Test mail</h1>
