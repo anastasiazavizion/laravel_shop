1fdf8088e9fc30a0e2f1307a35a63d13c53dec98
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-21 13:28:50 +0200

    Test mail

---
+++ resources/views/mail/test.blade.php
@@ -0,0 +1,10 @@
+<x-mail::message>
+    # Introduction
+
+    <x-mail::panel>
+     Test message
+    </x-mail::panel>
+
+    Thanks,<br>
+    {{ config('app.name') }}
+</x-mail::message>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('{any}', function () {
+
+    if(Auth::check()){
+        logs()->info('SEND MAIL');
+        Mail::to(\Illuminate\Support\Facades\Auth::user())->send(new \App\Mail\TestMail());
+    }
+
     return view('app');
 })->where('any', '.*');
