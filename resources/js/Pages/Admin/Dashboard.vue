47952bdfe803b84ec6446f54a281ce88d7f0d8ea
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-09-30 13:27:30 +0200

    Admin panel tabs

--- resources/js/Pages/Admin/Dashboard.vue
+++ resources/js/Pages/Admin/Dashboard.vue
@@ -7,6 +7,7 @@ import OrdersAmountByProducts from "@/Pages/Admin/Partials/OrdersAmountByProduct
 import OrdersAmountByCities from "@/Pages/Admin/Partials/OrdersAmountByCities.vue";
 import OrdersAmountByStatuses from "@/Pages/Admin/Partials/OrdersAmountByStatuses.vue";
 import OrdersAmountByCategories from "@/Pages/Admin/Partials/OrdersAmountByCategories.vue";
+import {Tab, TabGroup, TabList, TabPanel, TabPanels} from "@headlessui/vue";
 const store = useStore();
 
 const user = ref(null);
@@ -45,52 +46,97 @@ onMounted(async () => {
     })
 
 })
+
+const tabs = ref([
+    {name:'Statistic', active:true},
+    {name:'Map', active: false},
+]);
+
+const activePaymentType = ref()
+
+function setActiveTab(tab){
+    activePaymentType.value = tab.name;
+}
+
 </script>
 
 <template>
     <Header>Admin panel</Header>
     <div v-if="is('admin') && (user && !user.telegram_id)" id="telegram-widget-container"></div>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
-        <div class="adminCard">
-            <div class="text-base absolute left-5 top-5 font-normal">Orders</div>
-            <span>{{allOrdersAmount}}</span>
-        </div>
+    <TabGroup>
+        <TabList class="text-center grid grid-cols-2 mt-8 gap-8">
+            <Tab  class="tab"
+                  @click="setActiveTab(tab)"
+                  :class="[activePaymentType === tab.name ? 'active-tab' : '']"
+                 :key="tab.name"
+                 v-for="tab in tabs">
+                 {{ tab.name }}
+            </Tab>
+        </TabList>
 
-        <div class="adminCard">
-            <div class="text-base absolute left-5 top-5 font-normal">Products</div>
-            <span>{{allProductsAmount}}</span>
-        </div>
+        <TabPanels class="mt-4 min-h-40 sm:min-h-80">
+            <div>
+                <TabPanel  :key="tab.name"
+                           v-for="tab in tabs">
 
-        <div class="adminCard">
-            <div class="text-base absolute left-5 top-5 font-normal">Users</div>
-            <span>{{allUsersAmount}}</span>
-        </div>
+                    <div v-if="tab.name === 'Statistic'">
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
+                            <div class="adminCard">
+                                <div class="text-base absolute left-5 top-5 font-normal">Orders</div>
+                                <span>{{allOrdersAmount}}</span>
+                            </div>
 
-        <div class="adminCard">
-            <div class="text-base absolute left-5 top-5 font-normal">Paid Orders Income</div>
-            <span>{{ordersTotal}}$</span>
-        </div>
+                            <div class="adminCard">
+                                <div class="text-base absolute left-5 top-5 font-normal">Products</div>
+                                <span>{{allProductsAmount}}</span>
+                            </div>
 
-    </div>
+                            <div class="adminCard">
+                                <div class="text-base absolute left-5 top-5 font-normal">Users</div>
+                                <span>{{allUsersAmount}}</span>
+                            </div>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
-        <div>
-            <OrdersAmountByProducts/>
-        </div>
+                            <div class="adminCard">
+                                <div class="text-base absolute left-5 top-5 font-normal">Paid Orders Income</div>
+                                <span>{{ordersTotal}}$</span>
+                            </div>
 
-        <div>
-            <OrdersAmountByCities/>
-        </div>
+                        </div>
 
-        <div>
-            <OrdersAmountByStatuses/>
-        </div>
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
+                            <div>
+                                <OrdersAmountByProducts/>
+                            </div>
 
-        <div>
-            <OrdersAmountByCategories/>
-        </div>
+                            <div>
+                                <OrdersAmountByCities/>
+                            </div>
 
-    </div>
+                            <div>
+                                <OrdersAmountByStatuses/>
+                            </div>
+
+                            <div>
+                                <OrdersAmountByCategories/>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div v-if="tab.name === 'Map'">
+
+                        Map
+                    </div>
+
+                </TabPanel>
+            </div>
+
+        </TabPanels>
+    </TabGroup>
+
+
+
+
 
 </template>
