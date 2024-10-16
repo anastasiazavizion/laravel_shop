b8ab68c23ab2520a69ca78e6cf10fa436d78d949
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-10-16 12:50:36 +0200

    Installed Nova. Added Product respurce

---
+++ app/Nova/Product.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Nova;
+
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Currency;
+use Laravel\Nova\Fields\ID;
+use Laravel\Nova\Fields\Number;
+use Laravel\Nova\Fields\Slug;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Http\Requests\NovaRequest;
+
+class Product extends Resource
+{
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var class-string<\App\Models\Product>
+     */
+    public static $model = \App\Models\Product::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'title';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'id',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function fields(NovaRequest $request)
+    {
+        return [
+            ID::make()->sortable()->hideFromIndex(),
+            Slug::make('Slug')->from('Title')->readonly(true),
+            Text::make('Title')->required(),
+            Text::make('SKU')->required()->help('SKU for Product. Min 5 symbols'),
+            Currency::make('Price')->required(),
+            Number::make('Discount'),
+            Number::make('Quantity')->required(),
+            Text::make('Description'),
+
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function cards(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function filters(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function lenses(NovaRequest $request)
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @return array
+     */
+    public function actions(NovaRequest $request)
+    {
+        return [];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,7 @@
 <?php
-
-use App\Jobs\GeocodeAddressJob;
 use Illuminate\Support\Facades\Route;
 
-/*Route::get('/test', function () {
-
-    $order = \App\Models\Order::find(13);
-    $results = app("geocoder")
-        ->geocode("$order->city $order->address")
-        ->get();
-
-    $coordinates = $results[0]->getCoordinates();
-
-    dd($coordinates);
-
-
-
-    GeocodeAddressJob::dispatch($order);
-
-    die;
-
-});*/
-
+Route::redirect('login', 'nova/login')->name('login');
 
 Route::get('{any}', function () {
     return view('app');
