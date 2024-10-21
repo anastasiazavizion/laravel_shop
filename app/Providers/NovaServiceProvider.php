f05f25a711f06bac8402bad9ec9333b87d7030c5
Anastasia Zavizion <a.zavizion.a@gmail.com>
2024-10-21 21:38:48 +0200

    Custom Menu

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,8 +2,14 @@
 
 namespace App\Providers;
 
+use App\Enum\Role;
+use App\Nova\Category;
+use App\Nova\Dashboards\Main;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
@@ -17,6 +23,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function boot()
     {
         parent::boot();
+
+        $this->getCustomMenu();
 
 
         Nova::footer(function ($request) {
@@ -32,9 +40,9 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     protected function routes()
     {
         Nova::routes()
-                ->withAuthenticationRoutes()
-                ->withPasswordResetRoutes()
-                ->register();
+            ->withAuthenticationRoutes()
+            ->withPasswordResetRoutes()
+            ->register();
     }
 
     /**
@@ -46,11 +54,13 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
      */
     protected function gate()
     {
+
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
-                //
+                'hello@example.com',
             ]);
         });
+
     }
 
     /**
@@ -82,6 +92,32 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
      */
     public function register()
     {
+
+    }
+
+    private function getCustomMenu()
+    {
+        Nova::mainMenu(function (Request $request) {
+            return [
+                MenuSection::dashboard(Main::class)
+                    ->icon('chart-bar')
+                    ->withBadge('New', 'success'),
+                MenuSection::make('Products',[
+                    MenuItem::make('All Products','/resources/products'),
+                    MenuItem::make('Create Product','/resources/products/new'),
+                ])->icon('shopping-bag')->collapsable(),
+
+                MenuSection::resource(Category::class)->icon('tag'),
+
+                MenuSection::make('Users',[
+                    MenuItem::make('All Users','/resources/users'),
+                    MenuItem::make('Create User','/resources/users/new')->canSee(function ($request) {
+                        return $request->user()->hasRole(Role::ADMIN->value);
+                    }),
+                ])->icon('user')->collapsable(),
+
+            ];
+        });
 
     }
 }
